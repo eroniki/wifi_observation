1f5882f68e92cb00584ea2c6eaced2d18f2752ba
murat ambarkutuk <m.ambarkutuk@gmail.com>
2017-01-26 19:59:12 -0500

    Branch out for nm-tools

--- src/WifiHandler.py
+++ src/WifiHandler.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 
 import subprocess
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+'/python-nm-tool/')
+import nm_tool
 
 class WifiHandler(object):
     """docstring for WifiHandler"""
     def __init__(self):
         super(WifiHandler, self).__init__()
         self.response = None
-
+        print sys.path
     def query(self):
-        try:
-            self.response = subprocess.check_output(["iw", "dev", "wlan0", "scan"])
-        except subprocess.CalledProcessError:
-            print "No access to WiFi card, try running the script with sudo priviligies."
-        return self.response
+        self.response = nm_tool.get_dict()
+        return self.response['wlan0']['scan_results']
 
 def main():
     pass
--- src/get_observation.py
+++ src/get_observation.py
@@ -24,16 +24,19 @@ class wifi_obs(object):
         self.s = rospy.Service('wifi_observation', WifiArrayService, self.handle_wifi_obs)
         self.APList = list()
         self.wifi_handle = WifiHandler.WifiHandler()
-        self.text_parser = TextParser.TextParser()
+        # self.text_parser = TextParser.TextParser()
+        # TODO: Get the values as a parameters from ROS Param Server
+        self.prefix = "SENSORNET_CMS_"
+        self.nAP = 8
         rospy.loginfo("Ready to send wifi observations")
         rospy.spin()
 
-    def construct_response(self):
+    def construct_response(self, reading, prefix, number_AP):
         response = WifiArrayServiceResponse()
         obsList = list()
-        for i in range(len(self.APList)):
+        for i in range(number_AP):
             obs = WifiMessage()
-            obs.ssid = self.APList[i].SSID
+            obs.ssid = reading[prefix+i]
             obs.mac = self.APList[i].BSS
             obs.rss = self.APList[i].RSS
             obsList.append(obs)
@@ -51,10 +54,11 @@ class wifi_obs(object):
     def handle_wifi_obs(self, data):
         rospy.loginfo("Service request captured")
         self.reading =  self.wifi_handle.query()
-        self.text_parser.parse(self.reading)
-        self.APList = self.text_parser.APList
+        print self.reading
+        # self.text_parser.parse(self.reading)
+        # self.APList = self.text_parser.APList
         # self.text_parser.printAPList()
-        return self.construct_response()
+        return self.construct_response(self.reading, self.prefix, self.nAP)
 
 if __name__ == "__main__":
     wifi_obs = wifi_obs()
